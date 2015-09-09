16d02a99e4582f78da0e0a5a2ac900641ee34d55
kasai <kasai@P7484>
2015-09-09 13:27:52 +0900

    refuntoring

--- O-RMappingTest-ejb/src/java/ksist/ormapping/sample/BookOrderSessoinBean.java
+++ O-RMappingTest-ejb/src/java/ksist/ormapping/sample/BookOrderSessoinBean.java
@@ -30,34 +30,34 @@ public class BookOrderSessoinBean {
     public BookOrder createBookOrder(String customerName, List<Integer> bookIDs) {
         BookOrder bookOrder = null;
                         
-            // 注文の作成
-            bookOrder = new BookOrder();
-            bookOrder.setCustomerName(customerName);
-            bookOrder.setOrderDate(Calendar.getInstance());
+        // 注文の作成
+        bookOrder = new BookOrder();
+        bookOrder.setCustomerName(customerName);
+        bookOrder.setOrderDate(Calendar.getInstance());
 
-            // 注文明細リストの作成
-            List<BookOrderItem> bookOrderItems = new ArrayList<>();
-            for (int bookID : bookIDs) {
-                // 注文する書籍を取得する
-                Book book = em.find(Book.class, bookID);
+        // 注文明細リストの作成
+        List<BookOrderItem> bookOrderItems = new ArrayList<>();
+        for (int bookID : bookIDs) {
+            // 注文する書籍を取得する
+            Book book = em.find(Book.class, bookID);
 
-                // 発送予定日を取得し、書籍の在庫を減らす
-                // 注) トランザクションが終了すると書籍の在庫の減少も自動的に保存される
-                Calendar shippingDate = book.reserveShipping();
+            // 発送予定日を取得し、書籍の在庫を減らす
+            // 注) トランザクションが終了すると書籍の在庫の減少も自動的に保存される
+            Calendar shippingDate = book.reserveShipping();
 
-                // 注文明細を作成する
-                BookOrderItem bookOrderItem = new BookOrderItem();
-                bookOrderItem.setBook(book);
-                bookOrderItem.setShippingDate(shippingDate);
-                bookOrderItems.add(bookOrderItem);
-            }
+            // 注文明細を作成する
+            BookOrderItem bookOrderItem = new BookOrderItem();
+            bookOrderItem.setBook(book);
+            bookOrderItem.setShippingDate(shippingDate);
+            bookOrderItems.add(bookOrderItem);
+        }
 
-            // 注文に注文明細リストをセットする
-            bookOrder.setItems(bookOrderItems);
+        // 注文に注文明細リストをセットする
+        bookOrder.setItems(bookOrderItems);
 
-            // 注文を保存する(注文明細も同時に保存される)
-            em.persist(bookOrder);
-            return bookOrder;
+        // 注文を保存する(注文明細も同時に保存される)
+        em.persist(bookOrder);
+        return bookOrder;
     }
 
     // Add business logic below. (Right-click in editor and choose
--- O-RMappingTest-ejb/src/java/ksist/ormapping/sample/BookSessionBean.java
+++ O-RMappingTest-ejb/src/java/ksist/ormapping/sample/BookSessionBean.java
@@ -24,15 +24,27 @@ public class BookSessionBean {
     @PersistenceContext
     EntityManager em;
 
+    /**
+     * 書籍を全件取得する
+     * @return 書籍リスト
+     */
     public List<Book> findAll() {
         Query query = em.createNamedQuery("Book.findAll");
         return query.getResultList();
     }
     
+    /**
+     * 書籍を追加する
+     * @param book 
+     */
     public void add(Book book) {
         em.persist(book);
     }
     
+    /**
+     * 書籍を削除する
+     * @param id 書籍ID
+     */
     public void delete(int id) {
         Book book = em.find(Book.class, id);
         em.remove(book);
