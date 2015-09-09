16aafd7afe5918fb1b4a5016f8b418ce79cc283c
kasai <kasai@P7484>
2015-09-09 13:23:15 +0900

    new commit

---
+++ O-RMappingTest-war/src/java/ksist/ormapping/sample/servlet/BookOrderServlet.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ksist.ormapping.sample.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import ksist.ormapping.sample.BookOrderSessoinBean;
+
+/**
+ *
+ * @author kasai
+ */
+@WebServlet(name = "BookOrderServlet", urlPatterns = {"/bookOrder"})
+public class BookOrderServlet extends HttpServlet {
+
+    @EJB
+    BookOrderSessoinBean bean;
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        String customerName = request.getParameter("customerName");
+        String bookList[] = request.getParameterValues("bookList");
+        List<Integer> bookIDs = new ArrayList<>();
+        for (String bookID : bookList) {
+            bookIDs.add(Integer.parseInt(bookID));
+        }
+        
+        bean.createBookOrder(customerName, bookIDs);
+        
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/bookOrderFinish.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
